c5672adb2c0868087e48fcd37b08c83548cbd1f3
Fengda HUANG <h@phodal.com>
2015-08-01 23:06:56 +0800

    add remote js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const app = require('app');
 const BrowserWindow = require('browser-window');
+const Menu = require('menu');
 
 // report crashes to the Electron project
 require('crash-reporter').start();
@@ -49,4 +50,133 @@ app.on('activate-with-no-open-windows', function () {
 
 app.on('ready', function () {
 	mainWindow = createMainWindow();
+	initMenu();
 });
+
+function initMenu () {
+	var template = [
+		{
+			label: 'Iot Editor',
+			submenu: [
+				{
+					label: 'Iot Editor',
+					selector: 'orderFrontStandardAboutPanel:'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Services',
+					submenu: []
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Hide Electron',
+					accelerator: 'CmdOrCtrl+H',
+					selector: 'hide:'
+				},
+				{
+					label: 'Hide Others',
+					accelerator: 'CmdOrCtrl+Shift+H',
+					selector: 'hideOtherApplications:'
+				},
+				{
+					label: 'Show All',
+					selector: 'unhideAllApplications:'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Quit',
+					accelerator: 'CmdOrCtrl+Q',
+					selector: 'terminate:'
+				}
+			]
+		},
+		{
+			label: 'Edit',
+			submenu: [
+				{
+					label: 'Undo',
+					accelerator: 'CmdOrCtrl+Z',
+					selector: 'undo:'
+				},
+				{
+					label: 'Redo',
+					accelerator: 'Shift+CmdOrCtrl+Z',
+					selector: 'redo:'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Cut',
+					accelerator: 'CmdOrCtrl+X',
+					selector: 'cut:'
+				},
+				{
+					label: 'Copy',
+					accelerator: 'CmdOrCtrl+C',
+					selector: 'copy:'
+				},
+				{
+					label: 'Paste',
+					accelerator: 'CmdOrCtrl+V',
+					selector: 'paste:'
+				},
+				{
+					label: 'Select All',
+					accelerator: 'CmdOrCtrl+A',
+					selector: 'selectAll:'
+				}
+			]
+		},
+		{
+			label: 'View',
+			submenu: [
+				{
+					label: 'Reload',
+					accelerator: 'CmdOrCtrl+R',
+					click: function() { remote.getCurrentWindow().reload(); }
+				},
+				{
+					label: 'Toggle DevTools',
+					accelerator: 'Alt+CmdOrCtrl+I',
+					click: function() { remote.getCurrentWindow().toggleDevTools(); }
+				}
+			]
+		},
+		{
+			label: 'Window',
+			submenu: [
+				{
+					label: 'Minimize',
+					accelerator: 'CmdOrCtrl+M',
+					selector: 'performMiniaturize:'
+				},
+				{
+					label: 'Close',
+					accelerator: 'CmdOrCtrl+W',
+					selector: 'performClose:'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Bring All to Front',
+					selector: 'arrangeInFront:'
+				}
+			]
+		},
+		{
+			label: 'Help',
+			submenu: []
+		}
+	];
+
+	var menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
+}
