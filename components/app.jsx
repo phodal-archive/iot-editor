733281b5147ff42014a5b23adb45b3b8c2e349fa
Fengda HUANG <h@phodal.com>
2015-08-02 12:54:01 +0800

    use marterial ui

--- components/app.jsx
+++ components/app.jsx
@@ -4,68 +4,62 @@ import { RouteHandler, Route, Navigation } from 'react-router';
 import { Nav, Navbar } from 'react-bootstrap';
 import { NavItemLink } from 'react-router-bootstrap';
 import { HomePage, NodeMCUPage, DebugPage } from './pages';
+let mui = require('material-ui'),
+	AppBar = mui.AppBar;
+var ThemeManager = require('material-ui/lib/styles/theme-manager')();
+
+ThemeManager.setTheme(ThemeManager.types.DARK);
 
 var remote = window.require('remote');
 var runtime = remote.require('./core/runtime');
 
 var _routes = runtime.routes.map(function (r) {
-  var handler = window.require(r.handler);
-  return <Route key={r.route} name={r.route} handler={handler}/>;
-});
-
-var _navbar = runtime.routes.filter(function (r) {
-  return r.navbar;
+	var handler = window.require(r.handler);
+	return <Route key={r.route} name={r.route} handler={handler}/>;
 });
 
 const App = React.createClass({
-  mixins: [ Navigation ],
+	mixins: [Navigation],
+	childContextTypes: {
+		muiTheme: React.PropTypes.object
+	},
 
-  componentDidMount() {
-    var ipc = window.require('ipc');
-    ipc.on('transitionTo', function(routeName) {
-      //this.transitionTo(routeName, { the: 'params' }, { the: 'query' });
-      this.transitionTo(routeName);
-    }.bind(this));
-  },
+	getChildContext() {
+		return {
+			muiTheme: ThemeManager.getCurrentTheme()
+		};
+	},
+	componentDidMount() {
+		var ipc = window.require('ipc');
+		ipc.on('transitionTo', function (routeName) {
+			//this.transitionTo(routeName, { the: 'params' }, { the: 'query' });
+			this.transitionTo(routeName);
+		}.bind(this));
+	},
 
-  render() {
-    var links = _navbar.map(function (r) {
-      return (
-        <NavItemLink key={r.route} to={r.route}>{r.text}</NavItemLink>
-      );
-    });
-    return (
-      <div>
-        <Navbar fixedTop fluid>
-          <Nav>
-            <NavItemLink to="home">
-              <i className="fa fa-lg fa-home"></i>
-            </NavItemLink>
-            <NavItemLink to="nodemcu">
-              <i className="fa fa-lg fa-rocket"></i>
-            </NavItemLink>
-            <NavItemLink to="debug">
-              <i className="fa fa-lg fa-bug"></i>
-            </NavItemLink>
-            {links}
-          </Nav>
-        </Navbar>
+	render() {
+		return (
+			<div>
+				<AppBar
+					title="Menu"
+					iconClassNameRight="muidocs-icon-navigation-expand-more"
+					onLeftIconButtonTouchTap=""/>
 
-        <RouteHandler />
-      </div>
-    );
-  }
+				<RouteHandler />
+			</div>
+		);
+	}
 });
 
 var routes = (
-  <Route name="app" path="/" handler={App}>
-    <Route name="home" path="/" handler={HomePage} />
-    <Route name="debug" handler={DebugPage} />
-    <Route name="nodemcu" handler={NodeMCUPage} />
-    { _routes }
-  </Route>
+	<Route name="app" path="/" handler={App}>
+		<Route name="home" path="/" handler={HomePage}/>
+		<Route name="debug" handler={DebugPage}/>
+		<Route name="nodemcu" handler={NodeMCUPage}/>
+		{ _routes }
+	</Route>
 );
 
 Router.run(routes, function (Handler) {
-  React.render(<Handler />, document.body);
+	React.render(<Handler />, document.body);
 });
