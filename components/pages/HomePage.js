b53d6a2849484097cb1a8b40b52d5f8481a24252
Fengda HUANG <h@phodal.com>
2015-08-03 08:42:20 +0800

    move mqtt to protocols directory

--- components/pages/HomePage.js
+++ components/pages/HomePage.js
@@ -1,112 +1,15 @@
 import React from 'react';
-let mui = require('material-ui');
-let {
-	Avatar,
-	Card,
-	CardActions,
-	CardHeader,
-	CardText,
-	CardTitle,
-	FlatButton,
-	Snackbar,
-	TextField
-	} = mui;
-let ipc = window.require('ipc');
+import MQTT from '../protocols/mqtt.js';
 
 class HomePage extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			autoHideDuration: 0,
-			mqttServer: 'mqtt.phodal.com',
-			mqttTopic: 'test',
-			mqttMessage: 'Hello, MQTT',
-			serverMessage: ''
-		};
-
-		this.handleRequestServer = this.handleRequestServer.bind(this);
-		this.handleServerURLChange = this.handleServerURLChange.bind(this);
-		this.handleServerTopicChange = this.handleServerTopicChange.bind(this);
-		this.handleServerMessageChange = this.handleServerMessageChange.bind(this);
-	}
-
-	handleServerURLChange(event) {
-		this.setState({mqttServer: event.target.value});
-	}
-
-	handleServerTopicChange(event) {
-		this.setState({mqttTopic: event.target.value});
-	}
-
-	handleServerMessageChange(event) {
-		this.setState({mqttMessage: event.target.value});
-	}
-
 	render() {
-		var mqttServer = this.state.mqttServer;
-		var mqttTopic = this.state.mqttTopic;
-		var serverMessage = this.state.serverMessage;
-		var mqttMessage = this.state.mqttMessage;
 
 		return (
 			<div>
-				<Card initiallyExpanded={false}>
-					<CardHeader
-						title="MQTT"
-						subtitle="Message Queuing Telemetry Transport，消息队列遥测传输"
-						avatar={<Avatar style={{color:'red'}}>M</Avatar>}
-						showExpandableButton={true}>
-					</CardHeader>
-					<CardActions expandable={true}>
-						<TextField
-							hintText="MQTT服务器"
-							defaultValue={mqttServer}
-							floatingLabelText="MQTT服务器"
-							onChange={this.handleServerURLChange}
-							/>
-						<TextField
-							hintText="Topic"
-							defaultValue={mqttTopic}
-							onChange={this.handleServerTopicChange}
-							floatingLabelText="Topic"/>
-					</CardActions>
-
-					<CardActions expandable={true}>
-						<TextField
-							hintText="Message"
-							floatingLabelText="Message"
-							defaultValue={mqttMessage}
-							onChange={this.handleServerMessageChange}
-							multiLine={true}/>
-
-						<FlatButton label="确定" secondary={true} onClick={this.handleRequestServer}/>
-					</CardActions>
-				</Card>
-
-				<Snackbar
-					ref="snackbar"
-					message={serverMessage}
-					action="undo"
-					autoHideDuration={this.state.autoHideDuration} />
+				<MQTT />
 			</div>
 		);
 	}
-
-
-	handleRequestServer(e) {
-		var result = ipc.sendSync('mqtt', this.state);
-		if(result.connected) {
-			this.setState({serverMessage: 'connected successful'});
-			this.refs.snackbar.show();
-		}
-
-		var that = this;
-		ipc.on('synchronous-mqtt', function(arg) {
-			that.setState({serverMessage: arg});
-			that.refs.snackbar.show();
-		});
-	}
-
 }
 
 export default HomePage;
